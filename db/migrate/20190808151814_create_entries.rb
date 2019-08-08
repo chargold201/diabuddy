6a5c0e3dd5e0754b37663cb6cc6d319d9b4ca465
Charlotte Goldthwaite <chargold201@gmail.com>
2019-08-08 13:01:28 -0400

    Create migrations to create users and entries tables

---
+++ db/migrate/20190808151814_create_entries.rb
@@ -0,0 +1,12 @@
+class CreateEntries < ActiveRecord::Migration
+  def change
+    create_table :entries do |t|
+      t.integer :glucose
+      t.integer :insulin
+      t.integer :carbs
+      t.string :note
+      t.integer :user_id
+      t.timestamps
+    end
+  end
+end

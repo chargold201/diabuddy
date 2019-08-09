7141265ce2c45ded3f9cc858f78559ee744f5903
Charlotte Goldthwaite <chargold201@gmail.com>
2019-08-09 08:29:12 -0400

    Add helper methods for current user and logged in check

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -33,4 +33,14 @@ class SessionsController < ApplicationController
         session.clear
         redirect to '/'
     end
+
+    helpers do
+        def current_user
+            User.find(session[:user_id])
+        end
+    
+        def logged_in?
+            !!session[:user_id]
+        end
+    end
 end

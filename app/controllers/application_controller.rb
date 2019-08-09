a581d184babd68b2867b7861fec4f72f1cf3f0b0
Charlotte Goldthwaite <chargold201@gmail.com>
2019-08-09 09:08:38 -0400

    Move helper methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,13 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  helpers do
+    def current_user
+        User.find(session[:user_id])
+    end
+
+    def logged_in?
+        !!session[:user_id]
+    end
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -44,13 +44,4 @@ class SessionsController < ApplicationController
         redirect to '/'
     end
 
-    helpers do
-        def current_user
-            User.find(session[:user_id])
-        end
-    
-        def logged_in?
-            !!session[:user_id]
-        end
-    end
 end

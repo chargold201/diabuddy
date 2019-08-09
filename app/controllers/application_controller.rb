f45d0592765e1d77d17ba7bb3b211e1201983b91
Charlotte Goldthwaite <chargold201@gmail.com>
2019-08-09 17:52:59 -0400

    Change current user method to avoid re-searching the database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < Sinatra::Base
 
   helpers do
     def current_user
-        User.find(session[:user_id])
+        @current_user ||= User.find(session[:user_id])
     end
 
     def logged_in?

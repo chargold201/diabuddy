6587c118807ffb43ae5f109b0bc15265ac8ac919
Charlotte Goldthwaite <chargold201@gmail.com>
2019-08-09 09:06:31 -0400

    Edit entries index route for users not logged in

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,7 +1,11 @@
 class EntriesController < ApplicationController
 
     get '/entries' do
-        @entries = Entry.where(user_id: current_user.id)
-        erb :'/entries/index'
+        if logged_in?
+            @entries = Entry.where(user_id: current_user.id)
+            erb :'/entries/index'
+        else
+            redirect to '/'
+        end
     end
 end
